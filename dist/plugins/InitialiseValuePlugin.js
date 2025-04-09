991b7067fa91b95f893c5b91690ac54988cc93af
Alex Carter <alexc@harrap.it>
2025-04-09 11:19:09 +0100

    Updated InitialiseValuePlugin to parse html

--- dist/plugins/InitialiseValuePlugin.js
+++ dist/plugins/InitialiseValuePlugin.js
@@ -1,5 +1,6 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { $createParagraphNode, $createTextNode, $getRoot } from "lexical";
+import { $getRoot, $insertNodes } from "lexical";
+import { $generateNodesFromDOM } from '@lexical/html';
 import { useEffect, useState } from "react";
 export default function InitialiseValuePlugin({ src = null }) {
     const [editor] = useLexicalComposerContext();
@@ -16,13 +17,38 @@ export default function InitialiseValuePlugin({ src = null }) {
                 catch (e) {
                     // create a new editorState using the src value
                     editor.update(() => {
+                        //wrap new lines in a paragraph node
+                        const lines = src.split('\n');
+                        const paragraphs = lines.map(line => {
+                            const trimmed = line.trim();
+                            // check if this line is not already html
+                            if (trimmed.trim().startsWith('<') && trimmed.endsWith('>')) {
+                                return line;
+                            }
+                            return `<p>${line}</p>`;
+                        });
+                        const flattened = paragraphs.join('');
+                        // parse src as HTML
+                        const parser = new DOMParser();
+                        const dom = parser.parseFromString(flattened, 'text/html');
+                        // Once you have the DOM instance it's easy to generate LexicalNodes.
+                        const nodes = $generateNodesFromDOM(editor, dom);
+                        // Select the root
                         const root = $getRoot();
-                        // check if the root is empty
+                        // Insert them at a selection.
                         if (root.getFirstChild() === null) {
-                            const paragraph = $createParagraphNode();
-                            paragraph.append($createTextNode(src));
-                            root.append(paragraph);
+                            $insertNodes(nodes);
                         }
+                        //parse src as text
+                        // const root = $getRoot();
+                        // // check if the root is empty
+                        // if (root.getFirstChild() === null) {
+                        //     const paragraph = $createParagraphNode();
+                        //     paragraph.append(
+                        //         $createTextNode(src),
+                        //     );
+                        //     root.append(paragraph);
+                        // }
                     });
                 }
             }
